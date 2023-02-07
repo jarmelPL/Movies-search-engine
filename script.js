1701e6fa18ecfbfc7aeadb57b46624877f017020
Pawel Jarmula <pawel.jarmula@skf.com>
2023-02-07 14:13:17 +0100

    next basic features added

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 const titleInput = document.querySelector('.title')
 const btn = document.querySelector('.start')
+const moviesDiv = document.querySelector('.movies_list')
 const apiKEY = '680664962b614346e4f587e2fdbff113'
 let titleValue;
-let moviesInfo=[];
+let results=[];
+let moviesInfo={};
+let page=1
 
 function startApp() {
     titleValue=titleInput.value
 
-    let URL = `https://api.themoviedb.org/3/search/movie?api_key=${apiKEY}&language=en-US&query=${titleValue}&page=1&include_adult=true`
+    let URL = `https://api.themoviedb.org/3/search/movie?api_key=${apiKEY}&language=en-US&query=${titleValue}&page=${page}&include_adult=true`
 
     fetch(URL)
         .then(response => response.json())
-        .then(response => response.results)
         // .then(results => console.log(results))
-        .then(results => {
-            moviesInfo = results.map(movie => {
+        .then(response => {
+            results = response.results.map(movie => {
                 return {
                     title: movie.title,
                     image: 'https://image.tmdb.org/t/p/original'+movie.poster_path,
@@ -23,10 +25,31 @@ function startApp() {
                     date: movie.release_date
                 }
             })
+            moviesInfo= {
+                page: response.page,
+                movies: results,
+                total_pages: response.total_pages
+            }
             console.log(moviesInfo)
         })
         .catch(err => console.error(err));
-    }
+
+    addMovies(moviesInfo)
+}
+
+function addMovies(moviesInfo) {
+    const newUlList= document.createElement('ul')
+    moviesDiv.appendChild(newUlList)
+
+    newUlList.append(addMoviesToUl(moviesInfo))
+}
+
+function addMoviesToUl(moviesInfo) {
+    return results.map(item => {
+        const newLi = document.createElement('li')
+        newLi.textContent=item.title
+    })
+}
 
 btn.addEventListener('click',startApp)
 
